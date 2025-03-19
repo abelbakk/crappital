af2cb9f8ee99e862a9f723a870f89fa50458b600
abelbakk <abelbakk@gmail.com>
2025-03-19 20:20:05 +0100

    initialize express

---
+++ server/src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (_, res) => {
+    res.send(`Server running on port ${PORT}`);
+});
+
+export default app;
