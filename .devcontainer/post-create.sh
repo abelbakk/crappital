fe1a4d4a96f7bf0b6f35831de2ae53203a3c8338
abelbakk <abelbakk@gmail.com>
2025-11-15 23:57:24 +0100

    get frustrated and change devcontainer image

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
+set -e
 
-mkdir -p $HOME/.kube
-export KIND_EXPERIMENTAL_DOCKER_NETWORK="crappital-net"
-sudo kind create cluster --kubeconfig $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kind create cluster
 kubectl wait --for=condition=Ready node --all --timeout=300s
