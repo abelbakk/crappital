6e52d8ff6fc1c0c37f5f025cac62f2a7cd8cf2fa
abelbakk <abelbakk@gmail.com>
2025-11-15 23:33:01 +0100

    yet another attempt at kind

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 mkdir -p $HOME/.kube
-sudo kind create cluster --config .devcontainer/kind-config.yaml --kubeconfig $HOME/.kube/config
+sudo kind create cluster --network "crappital-net" --kubeconfig $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl wait --for=condition=Ready node --all --timeout=300s
-
-echo "cluster is ready"
