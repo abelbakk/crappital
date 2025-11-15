153c005e172939bb52a06f1275c5065142f56371
abelbakk <abelbakk@gmail.com>
2025-11-15 23:27:55 +0100

    another attempt to get kind running

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 mkdir -p $HOME/.kube
-sudo kind create cluster --kubeconfig $HOME/.kube/config
+sudo kind create cluster --config .devcontainer/kind-config.yaml --kubeconfig $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl wait --for=condition=Ready node --all --timeout=300s
+
+echo "cluster is ready"
