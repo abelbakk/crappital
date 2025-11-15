26e34ba849c81beb4a565441965268de7c270699
abelbakk <abelbakk@gmail.com>
2025-11-15 23:42:39 +0100

    one more attempt at kind

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p $HOME/.kube
-sudo kind create cluster --network "crappital-net" --kubeconfig $HOME/.kube/config
+export KIND_EXPERIMENTAL_DOCKER_NETWORK="crappital-net"
+sudo kind create cluster --kubeconfig $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl wait --for=condition=Ready node --all --timeout=300s
