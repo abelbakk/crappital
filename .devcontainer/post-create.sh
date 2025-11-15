9ab24a5a3853fc340c9edd3bd9f406c3e51e1859
abelbakk <abelbakk@gmail.com>
2025-11-16 00:12:04 +0100

    another attempt

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
+export KIND_EXPERIMENTAL_DOCKER_NETWORK="crappital-net"
 kind create cluster
+sed -i 's|server: https://127.0.0.1:.*|server: https://kind-control-plane:6443|g' /root/.kube/config
 kubectl wait --for=condition=Ready node --all --timeout=300s
