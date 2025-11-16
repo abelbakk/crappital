9b1e2a1847a482bbdef539a4f473e564681627ad
abelbakk <abelbakk@gmail.com>
2025-11-16 16:36:47 +0100

    try to allow jenkins to deploy to the cluster

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 set -e
 
+KUBECONFIG_PATH="$PWD/.kube-config"
 export KIND_EXPERIMENTAL_DOCKER_NETWORK="crappital-net"
-kind create cluster --config .devcontainer/kind-config.yaml
-sed -i 's|server: https://127.0.0.1:.*|server: https://kind-control-plane:6443|g' /root/.kube/config
-kubectl wait --for=condition=Ready node --all --timeout=300s
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-kubectl wait --namespace ingress-nginx \
+kind create cluster \
+    --config .devcontainer/kind-config.yaml \
+    --kubeconfig ${KUBECONFIG_PATH}
+sed -i 's|server: https://127.0.0.1:.*|server: https://kind-control-plane:6443|g' /root/.kube/config
+kubectl wait --kubeconfig ${KUBECONFIG_PATH} --for=condition=Ready node --all --timeout=300s
+
+kubectl apply --kubeconfig ${KUBECONFIG_PATH} \
+  -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl wait --kubeconfig ${KUBECONFIG_PATH} \
+  --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=300s
