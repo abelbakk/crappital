c01b4248baa2a49d4c3787ab2d3483b6211297de
abelbakk <abelbakk@gmail.com>
2025-11-16 16:40:58 +0100

    adjust post create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,7 +7,7 @@ export KIND_EXPERIMENTAL_DOCKER_NETWORK="crappital-net"
 kind create cluster \
     --config .devcontainer/kind-config.yaml \
     --kubeconfig ${KUBECONFIG_PATH}
-sed -i 's|server: https://127.0.0.1:.*|server: https://kind-control-plane:6443|g' /root/.kube/config
+sed -i 's|server: https://127.0.0.1:.*|server: https://kind-control-plane:6443|g' ${KUBECONFIG_PATH}
 kubectl wait --kubeconfig ${KUBECONFIG_PATH} --for=condition=Ready node --all --timeout=300s
 
 kubectl apply --kubeconfig ${KUBECONFIG_PATH} \
